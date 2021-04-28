8cd70b6d64a48ef4d3ae374a6a1b575c3a2c476f
sseans <41116820+sseans@users.noreply.github.com>
2021-04-28 17:29:10 +1000

    styling for nav menu

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Nav.css";
+import { MdWork } from "react-icons/md";
+import { HiCode } from "react-icons/hi";
+import { FaProjectDiagram } from "react-icons/fa";
+import { HiDocumentText } from "react-icons/hi";
 
 export default function Nav() {
   return (
@@ -8,16 +12,28 @@ export default function Nav() {
         <div className="nav-icon">Sean</div>
         <ul className="nav-menu">
           <li>
-            <button className="nav-button">Skills</button>
+            <button className="nav-button">
+              <HiCode />
+              Skills
+            </button>
           </li>
           <li>
-            <button className="nav-button">Projects</button>
+            <button className="nav-button">
+              <FaProjectDiagram />
+              Projects
+            </button>
           </li>
           <li>
-            <button className="nav-button">Experience</button>
+            <button className="nav-button">
+              <MdWork />
+              Experience
+            </button>
           </li>
           <li>
-            <button className="nav-resume-button">Resume</button>
+            <button className="nav-resume-button">
+              <HiDocumentText />
+              Resume
+            </button>
           </li>
         </ul>
       </div>
