e8b4dccbb9914aebc90ec289e42533dfcffdb7a5
sseans <41116820+sseans@users.noreply.github.com>
2021-04-29 15:15:20 +1000

    built custom hook for window sizing, styling for nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -5,36 +5,48 @@ import { HiCode } from "react-icons/hi";
 import { FaProjectDiagram } from "react-icons/fa";
 import { HiDocumentText } from "react-icons/hi";
 
+const menuItems = [
+  {
+    icon: <HiCode />,
+    title: "Skills",
+    link: "/skills",
+    className: "nav-button",
+  },
+  {
+    icon: <FaProjectDiagram />,
+    title: "Projects",
+    link: "/projects",
+    className: "nav-button",
+  },
+  {
+    icon: <MdWork />,
+    title: "Experience",
+    link: "/experience",
+    className: "nav-button",
+  },
+  {
+    icon: <HiDocumentText />,
+    title: "Resume",
+    link: "/resume",
+    className: "nav-resume-button",
+  },
+];
+
 export default function Nav() {
   return (
     <div className="nav">
       <div className="nav-wrapper">
-        <div className="nav-icon">Sean</div>
+        <div className="nav-icon">
+          SEAN<span>/</span>S.
+        </div>
         <ul className="nav-menu">
-          <li>
-            <button className="nav-button">
-              <HiCode />
-              Skills
-            </button>
-          </li>
-          <li>
-            <button className="nav-button">
-              <FaProjectDiagram />
-              Projects
-            </button>
-          </li>
-          <li>
-            <button className="nav-button">
-              <MdWork />
-              Experience
-            </button>
-          </li>
-          <li>
-            <button className="nav-resume-button">
-              <HiDocumentText />
-              Resume
-            </button>
-          </li>
+          {menuItems.map((x) => {
+            return (
+              <li>
+                <button className={x.className}>{x.title}</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
