c6ecd2ae9c7bb7cceae3e6d729bb4a51707e4db4
sseans <41116820+sseans@users.noreply.github.com>
2021-04-29 15:28:08 +1000

    started implementing mobile navigation

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 import { MdWork } from "react-icons/md";
 import { HiCode } from "react-icons/hi";
@@ -11,43 +11,61 @@ const menuItems = [
     title: "Skills",
     link: "/skills",
     className: "nav-button",
+    mobClassName: "nav-mobile-button",
   },
   {
     icon: <FaProjectDiagram />,
     title: "Projects",
     link: "/projects",
     className: "nav-button",
+    mobClassName: "nav-mobile-button",
   },
   {
     icon: <MdWork />,
     title: "Experience",
     link: "/experience",
     className: "nav-button",
+    mobClassName: "nav-mobile-button",
   },
   {
     icon: <HiDocumentText />,
     title: "Resume",
     link: "/resume",
     className: "nav-resume-button",
+    mobClassName: "nav-mobile-resume-button",
   },
 ];
 
 export default function Nav() {
+  const [mobileMenuActive, setMobileMenuActive] = useState(false);
+
   return (
     <div className="nav">
       <div className="nav-wrapper">
         <div className="nav-icon">
           SEAN<span>/</span>S.
         </div>
-        <ul className="nav-menu">
-          {menuItems.map((x) => {
-            return (
-              <li>
-                <button className={x.className}>{x.title}</button>
-              </li>
-            );
-          })}
-        </ul>
+        {mobileMenuActive === true ? (
+          <ul className="nav-mobile-menu">
+            {menuItems.map((x) => {
+              return (
+                <li>
+                  <button className={x.mobClassName}>{x.title}</button>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <ul className="nav-menu">
+            {menuItems.map((x) => {
+              return (
+                <li>
+                  <button className={x.className}>{x.title}</button>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     </div>
   );
