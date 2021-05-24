d6d3a7f62858aad3711674e6f0ced1f2dd6cc0ec
sseans <41116820+sseans@users.noreply.github.com>
2021-05-24 20:20:42 +1000

    major changes to projects and started making responsive

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,25 +1,8 @@
 import React from "react";
 import "./Projects.css";
-import movieFXStill from "../../Assets/MovieWebAppStill.jpg";
-import Project from "./IndividualProjects/Project";
-
-const projects = [
-  {
-    title: "MovieFx - Vanilla JS",
-    desc: "blah blah",
-    movieImageSrc: movieFXStill,
-  },
-  {
-    title: "Weather App - Vanilla JS",
-    desc: "blah blah",
-    movieImageSrc: movieFXStill,
-  },
-  {
-    title: "Eat My Shorts - React JS",
-    desc: "blah blah",
-    movieImageSrc: movieFXStill,
-  },
-];
+import movieFXStill from "../../Assets/MovieWebAppStill.JPG";
+import { IoMdOpen } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
 
 export default function Projects() {
   return (
@@ -29,10 +12,73 @@ export default function Projects() {
           <div className="title-bar-projects"></div>
           Projects
         </div>
-        <h2>What I've been doing.</h2>
-        {projects.map((project) => (
-          <Project key={project.title} projectData={project} />
-        ))}
+        <h2>What I've been doing...</h2>
+        <div className="project-container">
+          <img className="project-image" src={movieFXStill} alt="MovieFX" />
+          <div className="project-info">
+            <div className="project-title">MovieFX</div>
+            <div className="project-subtitle">Pure Vanilla Javascript.</div>
+            <div className="project-desc">
+              A Project to collate Movies and TV Shows from all different
+              streaming platforms using the IMDb API to pull ratings,
+              suggestions, descriptions, poster art and more...
+              <br />
+              <br />
+              The purpose of this project was to extend my skills in vanilla
+              javascript without relying on the tools of a framework, as helpful
+              as they may be.
+            </div>
+            <div className="project-tech">Vanilla JS - Sass - Fetch</div>
+            <div className="project-links">
+              <FaGithub />
+              <IoMdOpen />
+            </div>
+          </div>
+        </div>
+        <div className="project-container">
+          <img className="project-image" src={movieFXStill} alt="MovieFX" />
+          <div className="project-info">
+            <div className="project-title">MovieFX</div>
+            <div className="project-subtitle">Pure Vanilla Javascript.</div>
+            <div className="project-desc">
+              A Project to collate Movies and TV Shows from all different
+              streaming platforms using the IMDb API to pull ratings,
+              suggestions, descriptions, poster art and more...
+              <br />
+              <br />
+              The purpose of this project was to extend my skills in vanilla
+              javascript without relying on the tools of a framework, as helpful
+              as they may be.
+            </div>
+            <div className="project-tech">Vanilla JS - Sass - Fetch</div>
+            <div className="project-links">
+              <FaGithub />
+              <IoMdOpen />
+            </div>
+          </div>
+        </div>
+        <div className="project-container">
+          <img className="project-image" src={movieFXStill} alt="MovieFX" />
+          <div className="project-info">
+            <div className="project-title">MovieFX</div>
+            <div className="project-subtitle">Pure Vanilla Javascript.</div>
+            <div className="project-desc">
+              A Project to collate Movies and TV Shows from all different
+              streaming platforms using the IMDb API to pull ratings,
+              suggestions, descriptions, poster art and more...
+              <br />
+              <br />
+              The purpose of this project was to extend my skills in vanilla
+              javascript without relying on the tools of a framework, as helpful
+              as they may be.
+            </div>
+            <div className="project-tech">Vanilla JS - Sass - Fetch</div>
+            <div className="project-links">
+              <FaGithub />
+              <IoMdOpen />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
