209b45c7ce0f9cc12f0afcb3df9d252c7b43602a
sseans <41116820+sseans@users.noreply.github.com>
2021-05-25 19:24:45 +1000

    fix

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -6,8 +6,16 @@ export default function Experience() {
     <div className="experience">
       <div className="experience-wrapper">
         <div className="experience-title">
-          <div className="title-bar"></div>
+          <div className="title-bar-experience"></div>
           Experience
+        </div>
+        <div className="job-container">
+          <div className="job-logo"></div>
+          <div className="job-info">
+            <div className="info-title"></div>
+            <div className="info-desc"></div>
+            <div className="info-tech"></div>
+          </div>
         </div>
       </div>
     </div>
