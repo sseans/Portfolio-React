be164eadc3d5e209dfdb125f2e43d621aac817c1
sseans <41116820+sseans@users.noreply.github.com>
2021-05-30 17:42:58 +1000

    almost freakin there

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./Footer.css";
 import { IoIosArrowDown } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="arrow-container">
-        <IoIosArrowDown />
-      </div>
       <div className="footer-wrapper">
         <div className="footer-title">Contact Me!</div>
         <div className="footer-desc">
@@ -17,6 +16,27 @@ export default function Footer() {
         <a className="footer-button" href="mailto:s.seanseale@gmail.com">
           Contact
         </a>
+      </div>
+      <div className="footer-madeby">
+        <h2>Made from ground zero by Sean Seale</h2>
+        <div className="nav-icons-div">
+          <a
+            style={{ display: "table-cell" }}
+            href="https://github.com/sseans"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            style={{ display: "table-cell" }}
+            href="https://www.linkedin.com/in/sean-seale-ab07691a9/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
       </div>
     </div>
   );
